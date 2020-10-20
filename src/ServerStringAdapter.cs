f34329d795abfcd79df9be27ddad8178e0d60d76
권대선 <dsun@hanmail.net>
2020-10-20 19:32:17 +0900

    신규

---
+++ src/ServerStringAdapter.cs
@@ -0,0 +1,30 @@
+﻿using Atomus.Diagnostics;
+using System;
+using System.Linq;
+
+namespace Atomus.Service
+{
+    public class ServerStringAdapter : IServiceString
+    {
+        public ServerStringAdapter() { }
+        
+        string IServiceString.Request(string data)
+        {
+            try
+            {
+                //return (new ServerStringManager() as IServiceString).Request(data);
+                return (this.CreateInstance("Service") as IServiceString).Request(data);
+            }
+            catch (AtomusException exception)
+            {
+                DiagnosticsTool.MyTrace(exception);
+                return exception.ToString();
+            }
+            catch (Exception exception)
+            {
+                DiagnosticsTool.MyTrace(exception);
+                return exception.ToString();
+            }
+        }
+    }
+}
